a58bfdcfdee6bfa833024a6bff2a8a221793b884
Archibaald-dev <milo.lallement@yahoo.fr>
2020-11-10 16:59:18 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var Planets = ["Earth","Jupiter","Mars","Neptune","Uranus"];
+console.log(Planets[0],Planets[1],Planets[2],Planets[3],Planets[4]);
+
+var Hello = function() {
+    return "Hello World!"
+}
+console.log(Hello())
